64c1df5f5b3e28a7fcb9386ecd8b129581ebac7c
Kaloyan Doychinov <doichinov.kaloyan@gmail.com>
2024-04-05 08:24:10 +0200

    init

---
+++ generator.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crypto/rand"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"horse/wordlist"
+	"log"
+	"math/big"
+	"strings"
+)
+
+func getRandomInt(max int) int64 {
+	num, err := rand.Int(rand.Reader, big.NewInt(int64(max+1)))
+	if err != nil {
+		log.Fatalf("Failed to initialize secure random number generator! %s", err)
+	}
+	return num.Int64()
+}
+
+func getWord(WordList *[]string, capitalize bool) string {
+	vWords := *WordList
+	index := getRandomInt(len(vWords))
+	newWord := vWords[index]
+	if capitalize {
+		newWord = cases.Title(language.English, cases.Compact).String(newWord)
+	}
+	return newWord
+}
+
+func generate(minWords int, minLength int, separator string, capitalize bool) string {
+	var words []string
+	dictionary := &wordlist.FullDictionary
+	for i := 1; i <= minWords; i++ {
+		words = append(words, getWord(dictionary, capitalize))
+	}
+
+	password := strings.Join(words, separator)
+
+	// If password still doesn't meet the minimum length requirement, keep adding words until it does
+	for len(password) < minLength {
+		words = append(words, getWord(dictionary, capitalize))
+		password = strings.Join(words, separator)
+	}
+	return password
+}
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var minWordsFlag = flag.Int("w", 4, "Minimum number of words")
+var minLengthFlag = flag.Int("l", 20, "Minimum password length, including separator")
+var separatorFlag = flag.String("s", "", "Word separator")
+var capitalizeFlag = flag.Bool("c", false, "Capitalize first letter of each word")
+
+func main() {
+	flag.Parse()
+	res := generate(*minWordsFlag, *minLengthFlag, *separatorFlag, *capitalizeFlag)
+	fmt.Println(res)
+}
